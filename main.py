5b6aa3265c48813fd8fb11a591d86b1e191650a4
profdiegoaugusto <diaugustobarros@gmail.com>
2023-11-17 16:32:53 -0300

    Adiciona a classe de controle

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from flask import Flask
+
+from controllers.calculadora_controller import CalculadoraController
+
+app = Flask(__name__, template_folder="views/templates")
+
+app.register_blueprint(CalculadoraController.calculadoraBlueprint)
+
 
 if __name__ == '__main__':
-    pass
-
+    app.run(host="127.0.0.1", port=5000, debug=True)
